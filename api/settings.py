c3b866666fc3f4898c35ee35e0f0f15246413481
ehihameneromosele <ehihameneromosele@gmail.com>
2025-11-05 13:50:49 +0100

    install database

--- api/settings.py
+++ api/settings.py
@@ -97,7 +97,7 @@ if DEBUG:
 else:
     DATABASES = {
         'default': dj_database_url.config(
-            default=config('DATABASE_URL'),
+            default=os.getenv('DATABASE_URL'),
             conn_max_age=600,
             ssl_require=True
         )
